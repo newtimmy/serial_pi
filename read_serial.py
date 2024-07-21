7f8985f9be414bd1d8cb810f32d88df751d166e4
Timm Reich <timmreich@hotmail.de>
2024-07-21 19:47:24 +0200

    initial commit

--- read_serial.py
+++ read_serial.py
@@ -1,19 +1,28 @@
 import serial
 import time
 
-# Adjust this to the correct virtual serial port
-serial_port = '/dev/pts/3'
+# Use the default serial port
+serial_port = '/dev/serial0'
 baud_rate = 9600
+output_file = 'serial_data.txt'
 
 try:
+    # Open the serial port
     ser = serial.Serial(serial_port, baud_rate, timeout=1)
     print(f"Connected to {serial_port} at {baud_rate} baud")
 
-    while True:
-        if ser.in_waiting > 0:
-            data = ser.readline().decode('utf-8').rstrip()
-            print(f"Received: {data}")
-        time.sleep(1)
+    # Open the file in append mode
+    with open(output_file, 'a') as f:
+        while True:
+            if ser.in_waiting > 0:
+                # Read the data from the serial port
+                data = ser.readline().decode('utf-8').rstrip()
+                print(f"Received: {data}")
+
+                # Write the data to the file
+                f.write(f"{data}\n")
+                f.flush()  # Ensure the data is written to the file immediately
+            time.sleep(1)
 
 except serial.SerialException as e:
     print(f"Error: {e}")
@@ -22,5 +31,6 @@ except KeyboardInterrupt:
     print("Exiting program")
 
 finally:
+    # Close the serial port if it is open
     if ser.is_open:
         ser.close()
